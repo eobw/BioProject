e048cbc9324fc99964b4eb598d2d5380a06c3cae
Caitlin Vigetun Haughey <caithaug@rackham4.uppmax.uu.se>
2018-11-28 15:53:52 +0100

    Removed trinity files

---
+++ trinity_full_dataset/Trinity_bashscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH -A snic2018-8-339
+#SBATCH -p core
+#SBATCH -n 6
+#SBATCH -t 12:00:00
+#SBATCH -J RNA_assembly_job1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user caitlinvigetun@gmail.com
+# Load modules
+module load bioinfo-tools
+module load trinity/2.4.0
+module load conda/latest
+module load snakemake 
+module load samtools/1.8
+# Your commands
+snakemake
